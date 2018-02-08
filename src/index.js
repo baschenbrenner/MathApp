57a15dac0d6635fea0c71f7668a85a3ac7ec9f84
baschenbrenner <baschenbrenner@hotmail.com>
2018-02-08 09:37:50 -0600

    setting up basic structure

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import { App } from './App'
+
+const store = configureStore()
+
+ReactDOM.render(
+    <App store={store}/>,
+    document.getElementById('root')
+);
