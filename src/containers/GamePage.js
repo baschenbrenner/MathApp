e4c6eeddfa8c8ee3c2a801f7dbb5de0031131323
baschenbrenner <baschenbrenner@hotmail.com>
2018-02-09 07:42:30 -0600

    added gamepage

---
+++ src/containers/GamePage.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
+import { setNumbersForGame } from '../actions/gameActions';
+
+
+class GamePage extends Component {
+
+  render() {
+
+    const {match, game}= this.props
+    return (
+      <div>Game Page
+
+      </div>
+    )
+  }
+};
+
+const mapStateToProps = state => {
+  return {
+    game: state.game
+  };
+}
+
+export default connect(mapStateToProps, { setNumbersForGame })(GamePage);
