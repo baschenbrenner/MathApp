692bc54ef50308268a357b54168dbb96a9fe0b2b
baschenbrenner <baschenbrenner@hotmail.com>
2018-02-09 10:22:32 -0500

    added timer to move through array of numbers

--- src/containers/GamePage.js
+++ src/containers/GamePage.js
@@ -4,14 +4,43 @@ import { Route, Switch } from 'react-router-dom';
 import { setNumbersForGame } from '../actions/gameActions';
 
 
-class GamePage extends Component {
 
+class GamePage extends Component {
+  constructor() {
+      super();
+
+      this.increaseIndex = this.increaseIndex.bind(this)
+      
+      this.state = {
+        index: 0
+      };
+    }
+
+increaseIndex() {
+  this.setState({
+    index: this.state.index + 1
+  })
+
+}
+handleOnClick = () => {
+  this.props.setNumbersForGame()
+}
+
+componentDidUpdate(){
+  console.log("hey")
+  setTimeout(this.increaseIndex,5000)
+}
   render() {
 
     const {game}= this.props
     return (
       <div>Game Page
-
+      <br/>
+      Solve this:
+      <p>
+      {this.props.game.numberSetA[this.state.index]} x {this.props.game.numberSetB[this.state.index]}
+      </p>
+        <button onClick={this.handleOnClick}>Start Game</button>
       </div>
     )
   }
