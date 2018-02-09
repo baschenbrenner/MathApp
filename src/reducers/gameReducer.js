82a5b1785cfec5b0b4518c6bc3fd955d78c2088b
baschenbrenner <baschenbrenner@hotmail.com>
2018-02-09 07:32:05 -0600

    finished add numbers case

--- src/reducers/gameReducer.js
+++ src/reducers/gameReducer.js
@@ -1,11 +1,31 @@
-export default (state = [], action) => {
+export default (state = {
+  numberSetA: [],
+  numberSetB: [],
+  operation: "multiplication",
+  timePerQuestion: 5,
+  numberOfQuestions: 10,
+  numberCorrect: 0,
+  numberIncorrect: 0,
+  numberUnanswered: 0,
+  status: "unstarted"
+
+}, action) => {
   switch (action.type) {
 
-    case 'ADD_MOVIE':
-      return [ ...state, action.movie ];
+    case 'SET_NUMBERS_FOR_GAME':
+      return Object.assign({}, state, { numberSetA: [5,6,7,8,9,5,6,7,8,9], numberSetB: [7,8,6,6,7,8,9,8,7,9]})
+
+    case 'GET_NEXT_QUESTION':
+      return
+
+    case 'CHECK_ANSWER':
+      return
+
+    case 'FINISH_GAME':
+      return
 
     case 'FETCH_MOVIES':
-      return action.movies;
+      return 
 
     default:
       return state;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux'
 import { createStore } from 'redux';
 import mathReducer from './mathReducer'
+import gameReducer from './gameReducer'
 import thunk from 'redux-thunk';
 // we wrap store in a function for testing purposes
 export const configureStore = () => {
-  return createStore(mathReducers, applyMiddleware(thunk))
+  return createStore(rootReducer, applyMiddleware(thunk))
 }
 
-const rootReducer = combineReducers({mathReducer, gameReducer})
+const rootReducer = combineReducers({
+  math: mathReducer,
+  game: gameReducer})
